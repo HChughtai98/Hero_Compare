3f1f8bc9b7e818d11f8d007c8c97e1417741dce8
Josip Milan <josip.milan@aol.com>
2023-12-22 18:02:49 +0100

    Front

---
+++ character-compare/src/assets/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Welcome to ...</h1>
+      <p>Hello ...</p>
+      <ul>
+        <li>
+          <h2>Meet Our Team</h2>
+          <div>Josip ...</div>
+        </li>
+        <li>
+          <div>Haris ...</div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default AboutPage;
