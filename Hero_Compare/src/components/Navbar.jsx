e0f741be2dfad613ef8c543e370b82b97ec3625c
Josip Milan <josip.milan@aol.com>
2023-12-29 15:02:00 +0100

    changes

--- Hero_Compare/src/components/Navbar.jsx
+++ Hero_Compare/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styling/Navbar.css";
+import ironhackLogo from "../images/ironhackLogo.png";
 
 function Navbar() {
   return (
-    <nav>
-      <h1>Hero Compare</h1>
+    <nav className="header">
+      <div className="header_contents">
+        <Link to="/">
+          <img
+            src={ironhackLogo}
+            alt="Ironhack Logo"
+            className="ironhackLogo"
+          />
+        </Link>
+        <h1>Hero Compare</h1>
+        <div className="aboutPage">
+          <Link to="/about">About</Link>
+        </div>
+      </div>
     </nav>
   );
 }
