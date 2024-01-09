483d5108c04bcababe14835948c1166848a4a0a1
Josip Milan <josip.milan@aol.com>
2024-01-09 18:49:51 +0100

    InfoPage

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -5,45 +5,167 @@ function Info() {
   return (
     <div className="info">
       <div className="info-page">
-        <div className="header">
+        <div className="title-info-page">
           <h1>Information Page</h1>
         </div>
-
         {/* Dota Information */}
-        <section className="game-info">
+        <div className="dota-game-info">
           <h2>Dota</h2>
-          <p>Info about the Dota game...</p>
-        </section>
-        <section>
+          <p>
+            "Dota 2" is a complex, strategy-oriented game that is recognized as
+            one of the forerunners of the MOBA (Multiplayer Online Battle Arena)
+            genre. Developed and published by Valve Corporation, the game pits
+            two teams of five players against each other, with each team's goal
+            being to destroy the opposing team's "Ancient," a large structure
+            located within their base. The game is known for its deep gameplay
+            mechanics, vast array of heroes (over 100), and each hero's unique
+            abilities, which players can select at the start of each match. The
+            game is highly strategic, requiring team coordination, quick
+            decision-making, and real-time management of resources and
+            positioning. The map is split into three lanes, and the area in
+            between is filled with neutral monsters that can be killed for extra
+            gold and experience. Each player controls their hero and works to
+            gather resources, defeat enemy heroes, and push towards the enemy
+            base. Dota 2 also boasts one of the most significant and passionate
+            esports communities in the world, with The International, its annual
+            world championship, featuring some of the largest prize pools in
+            esports history. The game's complexity and high skill ceiling have
+            made it both a popular and enduring title in the competitive gaming
+            scene.
+          </p>
+        </div>
+        <div className="dota-roles">
           <h3>Roles in Dota</h3>
           <p>Describe the roles in Dota...</p>
-        </section>
-        <section>
+        </div>
+        <div className="dota-classes">
           <h3>Classes in Dota</h3>
           <p>Information about classes in Dota...</p>
-        </section>
-        <section>
+        </div>
+        <div className="dota-complexity">
           <h3>Hero Complexity in Dota</h3>
-          <p>Explaining the complexity of Dota heroes...</p>
-        </section>
+          <p>
+            In Dota 2, hero complexity refers to the difficulty level associated
+            with mastering a hero's abilities and playstyle. The complexity
+            level ranges from 1 to 3, indicating the hero's ease of use,
+            strategic depth, and the skill required to play effectively. These
+            levels serve as a guide for players to understand the demands of
+            playing each hero and to make informed choices that match their
+            skill and experience. Complexity Level 1: Heroes with a complexity
+            rating of 1 are considered the easiest to play. They typically have
+            straightforward abilities with clear-cut uses, making them ideal for
+            beginners. These heroes often have fewer active abilities and more
+            passive ones, reducing the need for quick reflexes and intricate
+            ability combos. They are forgiving of mistakes and require less
+            intricate knowledge of game mechanics, which helps new players focus
+            on learning the overall dynamics of Dota 2. Complexity Level 2:
+            Level 2 heroes offer a moderate challenge and are suitable for
+            players who have a grasp of the game's basics. These heroes might
+            feature abilities with more significant interactions, requiring
+            players to understand not only their own hero's skills but also how
+            they can affect allies and enemies in various situations. They may
+            have skill shots, targeted spells, or require specific positioning
+            to be most effective. Playing these heroes well often involves
+            making more strategic decisions and having better game sense.
+            Complexity Level 3: Heroes at complexity level 3 are the most
+            challenging to master and are recommended for experienced players.
+            They typically have abilities that require precise timing, quick
+            decision-making, and an advanced understanding of Dota 2's
+            mechanics. These heroes might have combos that need to be executed
+            in a specific sequence for maximum effect or have abilities that can
+            drastically change the outcome of a fight if used correctly.
+            High-complexity heroes often have a steeper learning curve, and
+            successfully utilizing them can depend on a player's ability to
+            manage multiple tasks simultaneously, such as item usage, ability
+            combos, and understanding of intricate game mechanics. Choosing a
+            hero with the appropriate complexity level is crucial for a player's
+            growth and enjoyment in Dota 2. As players become more familiar with
+            the game, they often progress to more complex heroes, which can
+            offer a more rewarding experience due to their depth and potential
+            for impactful plays.
+          </p>
+        </div>
 
         {/* LoL Information */}
-        <section className="game-info">
+        <div className="lol-game-info">
           <h2>LoL</h2>
-          <p>Info about the LoL game...</p>
-        </section>
-        <section>
+          <p>
+            "League of Legends" (LoL) is a dynamic and competitive MOBA
+            (Multiplayer Online Battle Arena) game that has cemented itself as a
+            cultural phenomenon in the world of esports. Developed and published
+            by Riot Games, it features a roster of over 140 champions, each with
+            unique abilities and playstyles. In the standard game mode, two
+            teams of five players compete to destroy the opposing team's Nexus,
+            which is protected by defensive structures scattered along three
+            lanes. Matches in LoL involve a delicate balance of strategy, skill,
+            and teamwork, with players choosing champions that fulfill different
+            roles such as damage dealers, tanks, supports, and assassins.
+            Players must manage their champion's development by accumulating
+            experience points, gold, and purchasing items that enhance their
+            abilities. The game's map, Summoner's Rift, is the battleground
+            where players vie for control through laning, jungling, pushing, and
+            team fights. The strategic depth, frequent updates, character
+            diversity, and vibrant community contribute to the game's massive
+            appeal. League of Legends is renowned for its accessibility to
+            newcomers while also providing depth for seasoned gamers. It's also
+            celebrated for its world-class esports ecosystem, with the annual
+            World Championship drawing millions of viewers, making it one of the
+            most watched and followed esports globally.
+          </p>
+        </div>
+        <div className="lol-roles">
           <h3>Roles in LoL</h3>
           <p>Describe the roles in LoL...</p>
-        </section>
-        <section>
+        </div>
+        <div className="lol-classes">
           <h3>Classes in LoL</h3>
           <p>Information about classes in LoL...</p>
-        </section>
-        <section>
+        </div>
+        <div className="lol-complexity">
           <h3>Hero Complexity in LoL</h3>
-          <p>Explaining the complexity of LoL heroes...</p>
-        </section>
+          <p>
+            In "League of Legends" (LoL), hero complexity, often referred to as
+            champion difficulty, is an indicator of how challenging a champion
+            is to play effectively. Similar to Dota 2's system, LoL's champion
+            difficulty can also be thought of as having levels from 1 to 3,
+            although in official terms, Riot Games uses a different scale. These
+            levels take into account the mechanical skills required, the
+            intricacy of ability combos, strategic depth, and the level of game
+            knowledge needed to play the champion well. Here's a breakdown of
+            the levels: Complexity Level 1: These champions are the most
+            accessible and are well-suited for new players or those looking to
+            master the fundamentals of the game. Their abilities are
+            straightforward, often with a focus on single-target spells or less
+            complex skill shots, and they usually lack intricate combo
+            sequences. Champions at this level tend to have more forgiving
+            gameplay, allowing players to learn the roles and objectives without
+            being heavily punished for mistakes. They provide a solid foundation
+            for understanding the core mechanics of the game. Complexity Level
+            2: Champions with a medium difficulty level require a better
+            understanding of the game's mechanics and more nuanced play to be
+            effective. They may have abilities that interact in more complex
+            ways, require better positioning, or involve skill shots that
+            require practice to land consistently. These champions often have a
+            combination of passive and active abilities that offer a moderate
+            level of depth in strategy and execution. Players need to have a
+            grasp of not just their own champion's kit, but also how to respond
+            to the abilities of opponents. Complexity Level 3: These are the
+            most challenging champions to play in LoL and demand a high level of
+            skill, timing, and strategic knowledge. They often feature abilities
+            with high outplay potential, including intricate combos,
+            transformations, or abilities with multiple uses or activation
+            phases. Playing these champions effectively typically involves
+            managing complex interactions between abilities, items, and
+            understanding nuanced aspects of the game like positioning, map
+            awareness, and enemy cooldowns. Such champions can be highly
+            rewarding for skilled players, offering significant impact in games
+            when mastered. Choosing a champion at the appropriate complexity
+            level is essential for player development in LoL. As players become
+            more adept at the game, they often experiment with more difficult
+            champions, enjoying the depth and potential for making impactful
+            plays that these champions provide.
+          </p>
+        </div>
       </div>
     </div>
   );
